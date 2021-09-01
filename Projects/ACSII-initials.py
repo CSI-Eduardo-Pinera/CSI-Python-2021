39963f8fc150013882d701db6071fe4a4d5836ab
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2021-09-01 14:36:09 -0400

    Project
    Project

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,7 @@
+print("EEEEE  DDDD   U   U     PPPP")
+print("E      D   D  U   U     P   P")
+print("E      D   D  U   U     P   P")
+print("EEE    D   D  U   U     PPPP")
+print("E      D   D  U   U     P")
+print("E      D   D  U   U     P")
+print("EEEEE  DDDD    UUU      P")
