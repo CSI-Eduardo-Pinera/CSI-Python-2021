2639723f8822ad9e9fd0c8107747865a5c14db32
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2021-10-20 15:04:58 -0400

    Shipping 2

--- Modules/Module5/Simpsonshipping.py
+++ Modules/Module5/Simpsonshipping.py
@@ -15,29 +15,29 @@ elif (weight > 10):
     print("Ground Shipping $", float(cost_ground))
 #courier shipping
 if (weight == 2 and weight < 2):
-    cost_ground = weight * 3.50 + 5.00
-    print("Courier Shipping $", float(cost_ground))
+    cost_courrier = weight * 3.50 + 5.00
+    print("Courier Shipping $", float(cost_courrier))
 elif (weight > 2 and weight <= 6):
-    cost_ground = weight * 7.00 + 8.00
-    print("Courier Shipping $", float(cost_ground))
+    cost_courrier = weight * 7.00 + 8.00
+    print("Courier Shipping $", float(cost_courrier))
 elif (weight > 6 and weight <= 10):
-    cost_ground = weight * 9.00 + 12.00
-    print("Courier Shipping $", float(cost_ground))
+    cost_courrier = weight * 9.00 + 12.00
+    print("Courier Shipping $", float(cost_courrier))
 elif (weight > 10):
-    cost_ground = weight * 10.50 + 15.00
-    print("Courier Shipping $", float(cost_ground))
+    cost_courrier = weight * 10.50 + 15.00
+    print("Courier Shipping $", float(cost_courrier))
 #Drone Shipping
 if (weight == 2 and weight < 2):
-    cost_ground = weight * 5.25 
-    print("Drone Shipping $", float(cost_ground))
+    cost_drone = weight * 5.25 
+    print("Drone Shipping $", float(cost_drone))
 elif (weight > 2 and weight <= 6):
-    cost_ground = weight * 10.50 
-    print("Drone Shipping $", float(cost_ground))
+    cost_drone = weight * 10.50 
+    print("Drone Shipping $", float(cost_drone))
 elif (weight > 6 and weight <= 10):
-    cost_ground = weight * 13.50 
-    print("Drone Shipping $", float(cost_ground))
+    cost_drone = weight * 13.50 
+    print("Drone Shipping $", float(cost_drone))
 elif (weight > 10):
-    cost_ground = weight * 15.75 
-    print("Drone Shipping $", float(cost_ground))
+    cost_drone = weight * 15.75 
+    print("Drone Shipping $", float(cost_drone))
 #Premium
 print("Premium Charge $150")
