9927b150e3c71de2420c0912b5d227b5da8cf002
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2021-10-28 14:32:21 -0400

    Update pistol

--- Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
+++ Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
@@ -1,11 +1,13 @@
-pistol = "GLOCK17"
-cartridge = "9x19mm"
-roundtype = "Single"
-velocity = 30
-building = "Soleil"
-building_height = 288
-gravity = 9.81
 import math
-time = math.sqrt((2 * building_height) / gravity)
-distance = (velocity * time)
-print(f"The gun selected was {pistol}, the cartidge used is {cartridge}, the round type is {roundtype}, and the velocity is {velocity}. The building chosen is {building}and it is {building_height} feet.")
+# pistol = "GLOCK17"
+# caliber = "9x19mm"
+# ammunition = "QuakeMaker"
+# velocity = 290
+# building = "Soleil"
+# building_height = 288
+# gravity = 9.81
+def projectileFunction(pistol:str, caliber:str, ammunition:str, velocity:int, building:str, building_height:int, gravity:int):
+    time = math.sqrt((2 * building_height) / gravity)
+    distance = (velocity * gravity)
+    print(f"The gun selected was {pistol}, the caliber used is {caliber}, the ammunition type is {ammunition}, and the velocity is {velocity}. The building chosen is {building} and it is {building_height} feet.")
+projectileFunction("GLOCK17", "9x19mm", "QuakeMaker", 290, "Soleil", 288, 9.81)
