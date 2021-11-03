14fc54cac2f46d60206b91d93504d6c5b82d686e
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2021-11-03 14:14:55 -0400

    update

--- Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
+++ Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
@@ -10,4 +10,15 @@ def projectileFunction(pistol:str, caliber:str, ammunition:str, velocity:int, bu
     time = math.sqrt((2 * building_height) / gravity)
     distance = (velocity * gravity)
     print(f"The gun selected was {pistol}, the caliber used is {caliber}, the ammunition type is {ammunition}, and the velocity is {velocity}. The building chosen is {building} and it is {building_height} feet.")
+
+experimentalData = {
+ "pistol" : "GLOCK17",
+ "caliber" : "9x19mm",
+ "ammunition" : "QuakeMaker",
+ "velocity" : 290,
+ "building" : "Soleil",
+ "building_height" : 288,
+ "gravity" : 9.81
+}
+
 projectileFunction("GLOCK17", "9x19mm", "QuakeMaker", 290, "Soleil", 288, 9.81)
