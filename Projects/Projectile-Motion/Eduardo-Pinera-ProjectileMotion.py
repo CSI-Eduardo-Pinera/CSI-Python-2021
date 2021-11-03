2521aee1c51792c76bdfaf915e0fe85be1102712
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2021-11-03 14:52:43 -0400

    ExperimentalData

--- Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
+++ Projects/Projectile-Motion/Eduardo-Pinera-ProjectileMotion.py
@@ -1,4 +1,5 @@
 import math
+from ExperimentalData import ExperimentalData
 # pistol = "GLOCK17"
 # caliber = "9x19mm"
 # ammunition = "QuakeMaker"
@@ -6,19 +7,22 @@ import math
 # building = "Soleil"
 # building_height = 288
 # gravity = 9.81
-def projectileFunction(pistol:str, caliber:str, ammunition:str, velocity:int, building:str, building_height:int, gravity:int):
+def projectileFunction(experimentalData: ExperimentalData):
     time = math.sqrt((2 * building_height) / gravity)
+    # distance = (experimentalData[velocity] * gravity)
     distance = (velocity * gravity)
     print(f"The gun selected was {pistol}, the caliber used is {caliber}, the ammunition type is {ammunition}, and the velocity is {velocity}. The building chosen is {building} and it is {building_height} feet.")
 
-experimentalData = {
- "pistol" : "GLOCK17",
- "caliber" : "9x19mm",
- "ammunition" : "QuakeMaker",
- "velocity" : 290,
- "building" : "Soleil",
- "building_height" : 288,
- "gravity" : 9.81
-}
+# experimentalData = {
+ 
+#  "pistol" : "GLOCK17",
+#  "caliber" : "9x19mm",
+#  "ammunition" : "QuakeMaker",
+#  "velocity" : 290,
+#  "building" : "Soleil",
+#  "building_height" : 288,
+#  "gravity" : 9.81
 
-projectileFunction("GLOCK17", "9x19mm", "QuakeMaker", 290, "Soleil", 288, 9.81)
+# }
+experimentalData = ExperimentalData("GLOCK17", "9x19mm", "QuakeMaker", 290, "Soleil", 288, 9.81)
+projectileFunction(experimentalData)
---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self, pistol:str, caliber:str, ammunition:str, velocity:int, building:str, building_height:int, gravity:int):
+        self.pistol = pistol
+        self.caliber = caliber
+        self.ammunition = ammunition
+        self.velocity = velocity
+        self.building = building
+        self.building_height = building_height
+        self.gravity = gravity 
