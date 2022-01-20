dd614fcac7679692f8f0fa10959b3169da685acb
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2022-01-20 14:53:38 -0400

    Create Eduardo-Pinera-Hangman1.py

---
+++ Projects/Eduardo-Pinera-Hangman1.py
@@ -0,0 +1,26 @@
+import random
+word_list = ["Monopoly", "Ajedrez", "Checkers", "Domino", "Solitario", "Clue", "Candy Land", "Operation", "Guess Who", "Uno"]
+def display_hangman(tries):
+    stages = ["""
+                    --------
+                    |       |
+                    |       O
+                    |      /|\\
+                    |       |
+                    |      /|\\ 
+                    |
+                    -
+                    """
+                       ,
+                    """
+                    --------
+                    |       |
+                    |       O
+                    |      /|\\ 
+                    |       |
+                    |      /| 
+                    -
+                    """
+                       ,
+                    """   
+                    ]
