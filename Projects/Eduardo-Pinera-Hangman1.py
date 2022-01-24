bfb85bb85de8b17c371547de6a93156cf513a200
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2022-01-24 14:41:10 -0400

    Update Eduardo-Pinera-Hangman1.py

--- Projects/Eduardo-Pinera-Hangman1.py
+++ Projects/Eduardo-Pinera-Hangman1.py
@@ -1,5 +1,31 @@
 import random
-word_list = ["Monopoly", "Ajedrez", "Checkers", "Domino", "Solitario", "Clue", "Candy Land", "Operation", "Guess Who", "Uno"]
+word_list = ["Monopoly", "Chess", "Checkers", "Domino", "Solitary", "Clue", "Candy Land", "Operation", "Guess Who", "Uno"]
+def get_word(word_list):
+   word = random.choice(word_list)
+   return word.upper()
+def play (word):
+   word_completion = "_" * len(word)
+   guessed = False
+   guessed_letters = []
+   guessed_words = []
+   tries = 6
+   print ("Let's play Hangman")
+   print("Theme : Board Games")
+   print(display_hangman(tries))
+   print(word_completion)
+   print("\n")
+   while not guessed and tries > 0:
+      guess = input("Guess a letter or word: "). upper()
+      if len(guess) == 1 and guess.isalpha():
+         if guess in guessed_letters:
+            print("You already tried", guess, "!")
+         elif guess not in word:
+            print(guess,"Isn't in the word")
+            tries -= 1
+            guessed_letters.append(guess)
+         else:
+            print("Nice one", guess, "is in the word!")
+            guessed_letters.append(guess)
 def display_hangman(tries):
     stages = ["""
                     --------
