1b5bd9cb5a1b3c0533d0ae2dde6e9b1f4ca089de
CSI-Eduardo-Pinera <89158829+CSI-Eduardo-Pinera@users.noreply.github.com>
2022-01-28 15:03:05 -0400

    Update Eduardo-Pinera-Hangman1.py

--- Projects/Eduardo-Pinera-Hangman1.py
+++ Projects/Eduardo-Pinera-Hangman1.py
@@ -4,7 +4,7 @@ def get_word(word_list):
    word = random.choice(word_list)
    return word.upper()
 def play (word):
-   word_completion = "_" * len(word)
+   word_completion = "-" * len(word)
    guessed = False
    guessed_letters = []
    guessed_words = []
@@ -26,8 +26,35 @@ def play (word):
          else:
             print("Nice one", guess, "is in the word!")
             guessed_letters.append(guess)
+            word__as_list = list(word_completion)
+            indices = [i for i, letter in  enumerate(word) if letter == guess]
+            for index in indices:
+               word__as_list[index] = guess
+            word_completion = "".join(word__as_list)
+            if "-" not in word_completion:
+               guessed = True
+      elif len(guess) == len(word) and guess.isalpha():
+         if guess in guessed_words:
+            print("You already tried", guess, "!")
+         elif guess != word:
+            print(guess, "Is not the word")
+            tries -= 1
+            guessed_words.append(guess)
+         else:
+            guessed = True
+            word_completion = word
+      else:
+         print("Invalid input")
+      print(display_hangman(tries))
+      print(word_completion)
+      print("\n")
+   if guessed:
+      print("Good job, you guessed the words.")
+   else:
+      print("I'm sorry, you lost. The word was" + word + "Maybe Next time!")
+
 def display_hangman(tries):
-    stages = ["""
+   stages = [ """
                     --------
                     |       |
                     |       O
@@ -36,8 +63,7 @@ def display_hangman(tries):
                     |      /|\\ 
                     |
                     -
-                    """
-                       ,
+                    """,
                     """
                     --------
                     |       |
@@ -96,5 +122,13 @@ def display_hangman(tries):
                     |       
                     |       
                     -
-                    """
-                    ]
+                    """,
+   ]
+   return stages[tries]
+def main():
+   word = get_word(word_list)
+   while input ("Again (Y/N)").upper() == "Y":
+      word = get_word(word_list)
+      play(word)
+if __name__ == "__main__":
+   main()
